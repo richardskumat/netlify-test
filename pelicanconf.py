71194f6c59634856b03b4de4508a1ffdaa8fd626
Richard Skumat <gitlab@richardskumat.com>
2023-03-09 15:11:30 +0000

    update this test site

--- pelicanconf.py
+++ pelicanconf.py
@@ -23,7 +23,7 @@ AUTHOR_FEED_RSS = None
 # Blogroll
 LINKS = (('Pelican', 'http://getpelican.com/'),
          ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),
+         ('Debian', 'https://www.debian.org/'),
          ('You can modify those links in your config file', '#'),)
 
 # Social widget
